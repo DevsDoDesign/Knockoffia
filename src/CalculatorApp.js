4a6099f2c0d046f825b31b2529cdd4ab5693dba7
Robb Lewis <me@therobb.com>
2015-06-25 16:07:39 +0100

    Calculator

--- src/CalculatorApp.js
+++ src/CalculatorApp.js
@@ -12,7 +12,8 @@ export default React.createClass({
 	getInitialState() {
 		return {
 			answer: 0,
-            operator: ''
+            operator: null,
+			answerTwo: 0
 		}
 	},
 	keyPressed(key) {
@@ -25,26 +26,114 @@ export default React.createClass({
 				}
 				else {
 					this.setState({
-						answer: 0
+						answer: 0,
+						answerTwo: 0
 					})
 				}
+				break;
 			case KEYS.ENTER:
 			case KEYS.HASH:
+				this.updateOperator()
+				break;
 			case KEYS.STAR:
+				this.calculate()
 				break;
 			default:
-				this.setState({
-					answer: this.state.answer += key
-				});
+				if (this.state.operator) {
+					this.setState({
+						answerTwo: this.state.answerTwo += key
+					});
+				}
+				else {
+					this.setState({
+						answer: this.state.answer += key
+					});
+				}
 				break;
 		}
 	},
-	clear() {
-		this.setState({ digits: '' })
+	updateOperator() {
+		this.setState({
+			answerTwo: 0
+		})
+		switch(this.state.operator) {
+			case null:
+				this.setState({
+					operator: '+'
+				})
+				break
+			case '+':
+				this.setState({
+					operator: '-'
+				})
+				break
+			case '-':
+				this.setState({
+					operator: '/'
+				})
+				break
+			case '/':
+				this.setState({
+					operator: 'x'
+				})
+				break
+			case '*':
+				this.setState({
+					operator: ''
+				})
+				break
+		}
+	},
+	calculate() {
+		switch(this.state.operator) {
+			case '+':
+				this.add()
+				break;
+			case '-':
+				this.minus()
+				break;
+			case '/':
+				this.divide()
+				break;
+			case '*':
+				this.multiply()
+				break;
+		}
+	},
+	add() {
+		this.setState({
+			answer: parseInt(this.state.answer) + parseInt(this.state.answerTwo),
+			operator: null
+		})
+	},
+	minus() {
+		this.setState({
+			answer: parseInt(this.state.answer) - parseInt(this.state.answerTwo),
+			operator: null
+		})
+	},
+	divide() {
+		this.setState({
+			answer: parseInt(this.state.answer) / parseInt(this.state.answerTwo),
+			operator: null
+		})
+	},
+	multiply() {
+		this.setState({
+			answer: parseInt(this.state.answer) * parseInt(this.state.answerTwo),
+			operator: null
+		})
+	},
+	getAnswer() {
+		if ( ! this.state.operator) {
+			return this.state.answer
+		}
+		
+		return this.state.answerTwo
 	},
 	render() {
 		return (
-		<div><div className="calc__operator">{this.state.operator}</div><div className="calc__answer">{this.state.answer}</div></div>
+		<div><div className="calc__operator">{this.state.operator}</div><div className="calc__answer">{this.getAnswer()}</div></div>
 		)
 	}
 })
