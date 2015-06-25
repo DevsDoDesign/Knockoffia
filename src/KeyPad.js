70b2308e8b56286796087af98673077a0f2e69a0
Dan Harper <intouch@danharper.me>
2015-06-25 15:12:36 +0100

    org

--- src/KeyPad.js
+++ src/KeyPad.js
@@ -15,21 +15,26 @@ export default React.createClass({
 	render() {
 		return (
 		<div>
-			<Key digit={KEYS.CLEAR} />
 			<Key digit={KEYS.UP} />
+			<Key digit={KEYS.CLEAR} />
+			<br/>
 			<Key digit={KEYS.DOWN} />
 			<Key digit={KEYS.ENTER} />
-			<Key digit="0" />
+			<br/>
 			<Key digit="1" />
 			<Key digit="2" />
 			<Key digit="3" />
+			<br/>
 			<Key digit="4" />
 			<Key digit="5" />
 			<Key digit="6" />
+			<br/>
 			<Key digit="7" />
 			<Key digit="8" />
 			<Key digit="9" />
+			<br/>
 			<Key digit="#" />
+			<Key digit="0" />
 			<Key digit="*" />
 		</div>
 		)
