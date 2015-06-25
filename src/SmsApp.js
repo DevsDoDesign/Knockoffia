f6ce132ff3e2aad6eb6cea275776371e967761f4
Alasdair Smith <ali@alasdairsmith.co.uk>
2015-06-25 15:08:45 +0100

    T9 working

--- src/SmsApp.js
+++ src/SmsApp.js
@@ -4,27 +4,18 @@ import Vent from './Vent'
 import KEYS from './KEYS'
 import { key2, key3, key4, key5, key6, key7, key8, key9, key0 } from './t9'
 
-var letters = new Map([
-	['2', key2()],
-	['3', key3()],
-	['4', key4()],
-	['5', key5()],
-	['6', key6()],
-	['7', key7()],
-	['8', key8()],
-	['9', key9()],
-	['0', key0()]
-]);
-
 export default React.createClass({
 	componentDidMount() {
 		Vent.onKeyPressed(this.keyPressed)
+
+		this.resetLetterMap();
 
 		this.previousKey = null;
 	},
 
 	componentWillUnmount() {
 		Vent.offKeyPressed(this.keyPressed)
+		clearTimeout(this.timeout);
 	},
 
 	getInitialState() {
@@ -41,7 +32,7 @@ export default React.createClass({
 				break;
 			case KEYS.CLEAR:
 				if (this.state.msg) {
-					this.clear();
+					this.del();
 				}
 				else {
 					Vent.exit()
@@ -54,35 +45,64 @@ export default React.createClass({
 
 				break;
 			default:
-				var letter = letters.get(key);
-				var val = letter.next().value;
+				var letter = this.getLetter(key);
 
-				if ( ! val) return;
-
+				// If cycling on same key, update the pending letter to current letter
 				if (key === this.previousKey) {
 					this.setState({
-						pendingLetter: val
+						pendingLetter: letter
 					});
 				}
+				// If NOT cycling on same key, set pending letter to current letter
+				// And append PREVIOUS pending letter to existing message
 				else {
 					this.setState({
-						pendingLetter: val,
+						pendingLetter: letter,
 						msg: this.state.msg + this.state.pendingLetter
 					})
 				}
 
 				this.previousKey = key;
 
-				setTimeout(function() {
+				// Set up timeout to "move" to next letter
+				this.timeout = setTimeout(function() {
+					// Append pending letter to message and remove it
 					this.setState({
 						pendingLetter: '',
 						msg: this.state.msg + this.state.pendingLetter
 					});
 
+					// Remove previous key - timeout has expired so we should reset. This allows adding of consecutive letters
 					this.previousKey = null;
+
+					// Reset map so that generators start back at beginning of sequence
+					this.resetLetterMap();
 				}.bind(this), 800);
 
 				break;
+		}
+	},
+
+	getLetter(key) {
+		// Get letter from map
+		var letter = this.letterMap.get(key);
+		var val = letter.next().value;
+
+		// If last letter in cycle, reset to start
+		if ( ! val)
+			val = letter.next(true).value;
+
+		return val;
+	},
+
+	del() {
+		if (this.state.pendingLetter) {
+			this.setState({ pendingLetter: '' });
+		}
+		else {
+			this.setState({
+				msg: this.state.msg.substring(0, this.state.msg.length -1)
+			})
 		}
 	},
 
@@ -93,9 +113,23 @@ export default React.createClass({
 		})
 	},
 
+	resetLetterMap() {
+		this.letterMap = new Map([
+			['2', key2()],
+			['3', key3()],
+			['4', key4()],
+			['5', key5()],
+			['6', key6()],
+			['7', key7()],
+			['8', key8()],
+			['9', key9()],
+			['0', key0()]
+		]);
+	},
+
 	render() {
 		return (
-		<div>Dialed: {this.state.msg + this.state.pendingLetter}</div>
+		<div>Message: {this.state.msg + this.state.pendingLetter}</div>
 		)
 	}
 });
